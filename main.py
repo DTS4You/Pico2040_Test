8466da953d786fed9d724f0656883b053aefff47
Norbert.Schwarz <info@datentechnikschwarz.de>
2023-03-29 20:47:32 +0200

    Default

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 # This is a sample Python script.
+import time
+from machine import Pin, Timer          # importing pin, and timer class
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+led = Pin(25, Pin.OUT)                   # GPIO14 as led output
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+led.value(False)                         # LED is off
 
 
-# Press the green button in the gutter to run the script.
+def timer_init_1(period_ms):
+    timer_1 = Timer(-1)
+    timer_1.init(period=period_ms, mode=Timer.PERIODIC, callback=toggle_led)
+    return timer_1
+
+
+def timer_init_2():
+    timer_2 = Timer(-1)
+    timer_2.init(period=500, mode=Timer.PERIODIC, callback=time_step)
+    return timer_2
+
+
+def timer_stop_1(timer_1):
+    timer_1.deinit()
+
+
+def timer_stop_2(timer_2):
+    timer_2.deinit()
+
+
+def time_step(timer_2):
+    print("Test")
+
+
+def toggle_led(timer_1):
+    led.value(not led.value())
+
+
+# -----------------------------------------------------------------------------
+def main_loop():
+
+    print("Main Loop")
+    t1 = timer_init_1(100)
+    t2 = timer_init_2()
+    try:
+        while True:
+            print("Wait_Loop")
+            time.sleep(1.0)
+    except KeyboardInterrupt:
+        timer_stop_1(t1)
+        timer_stop_2(t2)
+        led.value(False)
+        print("Main loop ended!")
+
+
+# =============================================================================
 if __name__ == '__main__':
-    print_hi("MyFirstTest_ABC")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main_loop()
+    print("=== End of Program ===")
+# =============================================================================
