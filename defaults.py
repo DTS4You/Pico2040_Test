cd726c6b17f3e0564cbefba5840d8d1c864ac688
norbert.schwarz <datentechnikschwarz@gmail.com>
2023-04-19 17:32:32 +0200

    Init

--- defaults.py
+++ defaults.py
@@ -2,6 +2,13 @@
 # MyGlobals
 ###############################################################################
 
+class Stripe:
+    num_of_stripes  = 8
+    num_of_leds     = 240                                  # Kann über die Segmente berechnet werden
+    pin_no          = [  0,  1,  2,  3,  4,  5,  6,  7]
+    pio_no          = [  0,  1,  2,  3,  4,  5,  6,  7]
+
+
 class Radar:
     num_of_beams    = 4
     # num_of_leds     = 78              # LEDs in Senderichtung
@@ -16,12 +23,82 @@ class Target:
 class Tracks:
     num_of_tracks   = 24
     num_of_leds     = 78
-    #                   1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
-    track_hit_x     = [ 0,  1,  2,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    # Direction -> True = Rechts nach Links -> False = Links nach Rechts
+    direction       = [ True,       # 1
+                        False,      # 2
+                        False,      # 3
+                        False,      # 4
+                        False,      # 5
+                        False,      # 6
+                        False,      # 6
+                        False,      # 7
+                        False,      # 8
+                        False,      # 9
+                        False,      # 10
+                        False,      # 11
+                        False,      # 12
+                        False,      # 13
+                        False,      # 14
+                        False,      # 15
+                        False,      # 16
+                        False,      # 17
+                        False,      # 18
+                        False,      # 19
+                        False,      # 20
+                        False,      # 21
+                        False,      # 22
+                        False,      # 23
+                        False]      # 24
     # Gibt die seitliche Position an, auf den der Radar-Strahls die Bahn treffen könnte
-    #                   1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
-    track_hit_y     = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
+    track_hit_x     = [  0,         # 1
+                         1,         # 2
+                         2,         # 3
+                         3,         # 4
+                         0,         # 5
+                         0,         # 6
+                         0,         # 7
+                         0,         # 8
+                         0,         # 9
+                         0,         # 10
+                         0,         # 11
+                         0,         # 12
+                         0,         # 13
+                         0,         # 14
+                         0,         # 15
+                         0,         # 16
+                         0,         # 17
+                         0,         # 18
+                         0,         # 19
+                         0,         # 20
+                         0,         # 21
+                         0,         # 22
+                         0,         # 23
+                         0]         # 24
     # Gibt die Höhe an, auf der der Rader-Strahl die Bahn treffen könnte
+    track_hit_y     = [ 20,         # 1
+                        20,         # 2
+                        20,         # 3
+                        20,         # 4
+                        20,         # 5
+                        20,         # 6
+                        20,         # 7
+                        20,         # 8
+                        20,         # 9
+                        20,         # 10
+                        20,         # 11
+                        20,         # 12
+                        20,         # 13
+                        20,         # 14
+                        20,         # 15
+                        20,         # 16
+                        20,         # 17
+                        20,         # 18
+                        20,         # 19
+                        20,         # 20
+                        20,         # 21
+                        20,         # 22
+                        20,         # 23
+                        20]         # 24
 
 
 class Colors:
